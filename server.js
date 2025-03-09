5c7ab6c92df3157977d7904ed74bb5ac0e0dc4f1
Earnest Piolo Fano <earnestpiolofano.developer@gmail.com>
2025-03-09 23:49:30 +0800

    Added EJS Package for the Views

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 require("dotenv").config();
 
 const express = require("express");
+const authRoute = require("./routes/auth-route");
 
 const app = express();
 
+app.set("view engine", "ejs");
+
 app.use(express.urlencoded({ extended: false }));
+app.use("/", authRoute);
 
 app.listen(process.env.PORT);
