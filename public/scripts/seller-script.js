99362a74fa6b4c1c998deb2268fd9ddb7f7943e8
Earnest Piolo Fano <earnestpiolofano.developer@gmail.com>
2025-03-24 05:03:32 +0800

    Added Modal Design

---
+++ public/scripts/seller-script.js
@@ -0,0 +1,11 @@
+const addProduct = document.getElementById("addProduct");
+const addProductModalClose = document.getElementById("addProductModalClose");
+const addProductModal = document.getElementById("addProductModal");
+
+addProduct.onclick = (event) => {
+    addProductModal.classList.add("active");
+};
+
+addProductModalClose.onclick = (event) => {
+    addProductModal.classList.remove("active");
+};
